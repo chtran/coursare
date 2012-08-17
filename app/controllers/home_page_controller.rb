781bd3868488dc715276d6cb3a62e9a5b8c304b3
Chau Tran <chau_tran@brown.edu>
2012-08-17 14:03:55 +0700

    Twitter bootstrap

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,2 +1,9 @@
 class HomePageController < ApplicationController
+  def dashboard
+    if !user_signed_in?
+      redirect_to new_user_session_path
+    else
+      render text: "OK", status: "200"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Coursare::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'homepage#index'
+  root :to => 'home_page#dashboard'
 
   # See how all your routes lay out with "rake routes"
 
