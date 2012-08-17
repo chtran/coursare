c3fa69905e0469078b1666ddaa65d86c277fa173
hoangtuananh <ahoang@bowdoin.edu>
2012-08-17 21:34:43 +0700

    working on home page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  def dashboard
+    # If user has not signed in, redirect to home page
+    if !user_signed_in?
+      redirect_to index_url,alert: "You have not signed in. Please sign in or join our community!"
+    else
+      redirect_to courses_url
+    end
+  end
+
+end
