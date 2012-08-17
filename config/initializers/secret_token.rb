e9b7fc43cdb516c69da9200ce7c3304b03c0412f
Chau Tran <chau_tran@brown.edu>
2012-08-17 13:39:29 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coursare::Application.config.secret_token = 'dfc50ee3118a525c2e0c60935376bd5156630195dce30d69b77247119df4f662110db05b38cdbcf8d757b326335eb872be883fc821082c66fcd9f15af01991f5'
